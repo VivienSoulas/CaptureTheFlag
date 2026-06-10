4445efd0012183a22c55b33ceab689aa7ea79063
VivienSoulas <vivien.soulas@gmail.com>
2026-06-10 20:12:50 +0200

    my_git

---
+++ picoCTF2026/General_skills/my_git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "git config user.name "root""
+echo "git config user.email "root@picoctf""
+echo "touch flag.txt"
+echo "git add flag.txt"
+echo "git commit -m "Adding flag.txt""
+echo "git push"
