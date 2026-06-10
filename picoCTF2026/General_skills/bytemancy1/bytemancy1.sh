e36625e72532af877697be6ec63b048391be9130
VivienSoulas <vivien.soulas@gmail.com>
2026-06-10 20:45:12 +0200

    undo.sh

--- picoCTF2026/General_skills/bytemancy1/bytemancy1.sh
+++ picoCTF2026/General_skills/bytemancy1/bytemancy1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#python3 -c 'print("e"*1751)'
-#$1 should be given as an argument the script corresponding at the netcat port number
+#$1 should be given as an argument to the script
+# it corresponds to the netcat port number
+
 
 python3 -c 'print("e"*1751)' | nc foggy-cliff.picoctf.net $1
