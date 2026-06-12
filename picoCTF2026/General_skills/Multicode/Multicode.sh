67e07a770184a953a42af807c5adca6237d4b511
VivienSoulas <vivien.soulas@gmail.com>
2026-06-12 21:13:22 +0200

    modified Multicode.sh

--- picoCTF2026/General_skills/Multicode/Multicode.sh
+++ picoCTF2026/General_skills/Multicode/Multicode.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# need message.txt to be redirected to Multicode.sh
 cat - | base64 -d | xxd -r -p | python3 -c "import sys, urllib.parse; print(urllib.parse.unquote(sys.stdin.read()))" | tr 'A-Za-z' 'N-ZA-Mn-za-m'
